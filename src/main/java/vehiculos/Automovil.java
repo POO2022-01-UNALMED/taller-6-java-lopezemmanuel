dc392d74cb1d969a2fc70dfb3b5c18c76201ad7c
Emmanuel López <lopezrodemmanuel@gmail.com>
2022-05-06 20:17:26 -0500

    Correciones

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -4,8 +4,8 @@ public class Automovil extends Vehiculo{
     private int puestos;
     private static int cantidad;
 
-	public Automovil(String placa, int puertas, float velocidadMaxima, String nombre, int precio, float peso, String traccion, Fabricante fabricante, int puestos) {
-		super(placa, 4, velocidadMaxima, nombre, precio, peso, "FWD", fabricante);
+	public Automovil(String placa, String nombre, int precio, float peso, Fabricante fabricante, int puestos) {
+		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
 		this.puestos = puestos;
 		
 		Automovil.cantidad++;
--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -4,7 +4,7 @@ public class Camioneta extends Vehiculo{
 	private boolean volco;
 	private static int cantidad;
 	
-	public Camioneta(String placa, int puertas, float velocidadMaxima, String nombre, int precio, float peso, String traccion, Fabricante fabricante, boolean volco) {
+	public Camioneta(String placa, int puertas, String nombre, int precio, float peso, Fabricante fabricante, boolean volco) {
 		super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
 		this.volco = volco;
 		
